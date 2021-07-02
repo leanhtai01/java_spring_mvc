58a4a775f54b62563efa036177dd632b9584bb5c
leanhtai01 <leanhtai01@gmail.com>
2021-07-02 10:42:56 +0700

    change package for IOrderRepository

--- SalesManagementWebApp/src/main/java/com/team18/salesmanagement/data/orderdetail/IOrderDetailRepository.java
+++ SalesManagementWebApp/src/main/java/com/team18/salesmanagement/data/orderdetail/IOrderDetailRepository.java
@@ -3,7 +3,7 @@
 // Author: 1760169 - Le Anh Tai
 // Email: leanhtai01@gmail.com
 // GitHub: https://github.com/leanhtai01
-package com.team18.salesmanagement.data.order;
+package com.team18.salesmanagement.data.orderdetail;
 
 import com.team18.salesmanagement.domain.orderdetail.OrderDetail;
 
