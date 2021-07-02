da1d8cc1586f57f307e7ca35ca6b88b1e1c45f08
leanhtai01 <leanhtai01@gmail.com>
2021-07-02 10:40:05 +0700

    add interface IOrderDetailRepository

---
+++ SalesManagementWebApp/src/main/java/com/team18/salesmanagement/data/order/IOrderDetailRepository.java
@@ -0,0 +1,12 @@
+// File: IOrderDetailRepository.java
+// Define data access operation for Order Detail
+// Author: 1760169 - Le Anh Tai
+// Email: leanhtai01@gmail.com
+// GitHub: https://github.com/leanhtai01
+package com.team18.salesmanagement.data.order;
+
+import com.team18.salesmanagement.domain.orderdetail.OrderDetail;
+
+public interface IOrderDetailRepository {
+    void insert(OrderDetail orderDetail);
+}
