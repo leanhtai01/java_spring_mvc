f99510da25880abb964abb3944f142043c2981ff
leanhtai01 <leanhtai01@gmail.com>
2021-07-06 20:54:20 +0700

    config: add resources handler

--- SalesManagementWebApp/src/main/java/com/team18/salesmanagement/config/WebConfig.java
+++ SalesManagementWebApp/src/main/java/com/team18/salesmanagement/config/WebConfig.java
@@ -12,6 +12,7 @@ import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation
     .DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -37,5 +38,11 @@ public class WebConfig implements WebMvcConfigurer {
     public void configureDefaultServletHandling(
         DefaultServletHandlerConfigurer configurer) {
 	configurer.enable();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations("/resources/");
     }
 } // end class WebConfig
--- SalesManagementWebApp/src/main/webapp/WEB-INF/flows/productpurchase/order/showOrder.jsp
+++ SalesManagementWebApp/src/main/webapp/WEB-INF/flows/productpurchase/order/showOrder.jsp
@@ -13,6 +13,7 @@
 <html>
     <head>
         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <link rel="stylesheet" href="resources/css/main.css">
         <title>Order Detail</title>
     </head>
     <body>
